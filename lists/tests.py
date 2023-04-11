cb7bec583917c12fb9e12ed75385362e70ccf57f
Michael Knott <michaelknott@live.co.uk>
2023-04-11 15:22:34 +0100

    Refactor functional_tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,8 @@
-
 def test_uses_home_page_template(client):
     response = client.get("/")
     assert response.templates[0].name == "home.html"
+
+
+def test_can_save_a_POST_request(client):
+    response = client.post("/", data={"item_text": "A new list item"})
+    assert "A new list item" in response.content.decode()
