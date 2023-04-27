9b3bcb16988d6ac00a990137c5f8237fb1376ab3
Michael Knott <michaelknott@live.co.uk>
2023-04-27 15:49:09 +0100

    New url and view for adding to existing lists. FT passes

--- lists/models.py
+++ lists/models.py
@@ -7,4 +7,6 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default="")
-    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+    list = models.ForeignKey(
+        List, default=None, related_name="items", on_delete=models.CASCADE
+    )
--- lists/tests.py
+++ lists/tests.py
@@ -23,9 +23,9 @@ def test_can_save_a_POST_request(client):
 @pytest.mark.django_db
 def test_redirects_after_POST(client):
     response = client.post("/lists/new", data={"item_text": "A new list item"})
-
+    new_list = List.objects.first()
     assert response.status_code == 302
-    assert response["location"] == "/lists/the-only-list-in-the-world/"
+    assert response["location"] == f"/lists/{new_list.id}/"
 
 
 @pytest.mark.django_db
@@ -66,18 +66,65 @@ def test_only_saves_items_when_necessary(client):
 
 @pytest.mark.django_db
 def test_uses_list_template(client):
-    response = client.get("/lists/the-only-list-in-the-world/")
+    list_ = List.objects.create()
+    response = client.get(f"/lists/{list_.id}/")
 
     assert response.templates[0].name == "list.html"
 
 
 @pytest.mark.django_db
-def test_displays_all_list_items(client):
-    list_ = List.objects.create()
-    Item.objects.create(text="itemey 1", list=list_)
-    Item.objects.create(text="itemey 2", list=list_)
+def test_displays_only_items_for_that_list(client):
+    correct_list = List.objects.create()
+    Item.objects.create(text="itemey 1", list=correct_list)
+    Item.objects.create(text="itemey 2", list=correct_list)
 
-    response = client.get("/lists/the-only-list-in-the-world/")
+    other_list = List.objects.create()
+    Item.objects.create(text="other list item 1", list=other_list)
+    Item.objects.create(text="other list item 2", list=other_list)
+
+    response = client.get(f"/lists/{correct_list.id}/")
 
     assert "itemey 1" in response.content.decode()
     assert "itemey 2" in response.content.decode()
+    assert "other list item 1" not in response.content.decode()
+    assert "other list item 2" not in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_can_save_a_POST_request_to_an_existing_list(client):
+    other_list = List.objects.create()  # noqa: F841
+    correct_list = List.objects.create()
+
+    client.post(
+        f"/lists/{correct_list.id}/add_item",
+        data={"item_text": "A new item for an existing list"},
+    )
+
+    assert Item.objects.count() == 1
+    new_item = Item.objects.first()
+    assert new_item.text == "A new item for an existing list"
+    assert new_item.list == correct_list
+
+
+@pytest.mark.django_db
+def test_redirects_to_list_view(client):
+    other_list = List.objects.create()  # noqa: F841
+    correct_list = List.objects.create()
+
+    response = client.post(
+        f"/lists/{correct_list.id}/add_item",
+        data={"item_text": "A new item for an existing list"},
+    )
+
+    assert response.status_code == 302
+    assert response["location"] == f"/lists/{correct_list.id}/"
+
+
+@pytest.mark.django_db
+def test_passes_correct_list_to_template(client):
+    other_list = List.objects.create()  # noqa: F841
+    correct_list = List.objects.create()
+
+    response = client.get(f"/lists/{correct_list.id}/")
+
+    assert response.context["list"] == correct_list
