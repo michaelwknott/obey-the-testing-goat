3b8872da0cc25afb9fe3a133a42080fc199b933d
Michael Knott <michaelknott@live.co.uk>
2023-05-11 16:37:12 +0100

    Amend settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "superlists.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "database/db.sqlite3",
+        "NAME": BASE_DIR / "../database/db.sqlite3",
     }
 }
 
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "../static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
