9c8e26738397dc44638ffaedd357dac808b4a900
Michael Knott <michaelknott@live.co.uk>
2023-05-16 11:09:49 +0100

    Update get server url fixture

--- functional_tests/conftest.py
+++ functional_tests/conftest.py
@@ -12,4 +12,7 @@ os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
 @pytest.fixture(scope="session", name="server_url")
 def get_server_url(live_server):
     """Start a django server to test against and return the url of the server."""
-    yield live_server.url
+    if staging_server := os.environ.get("STAGING_SERVER"):
+        yield f"http://{staging_server}"
+    else:
+        yield live_server.url
